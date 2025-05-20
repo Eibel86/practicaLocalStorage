274c0e4753a2fbfc31730cf82d984e7dd2c4d861
Eibel86 <afc72455@gmail.com>
2025-05-20 17:17:31 +0200

    funcionalidad eliminar productos terminado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 const formulario = document.querySelector("#formulario");
+const tabla = document.querySelector("#tabla")
 let nombreProducto = "";
 let idProducto= "";
 let arrProductos = JSON.parse(localStorage.getItem("producto")) || [];
@@ -38,12 +39,56 @@ formulario.addEventListener("submit", (ev) => {
   } else {
     console.log("nombre coincide, no se crea objeto")
   }
-  localStorage.setItem("producto", JSON.stringify(arrProductos)); 
+  localStorage.setItem("producto", JSON.stringify(arrProductos));
+  
+  crearTablaProducto();
+
 })
+
+tabla.addEventListener("click", (ev) => {
+      console.log(ev.target.id);
+      arrProductos.forEach((item, index, array) => {
+        if (item.id === ev.target.id && item.cantidad > 1){
+          item.cantidad -= 1;
+        } else if (item.id === ev.target.id && item.cantidad <= 1){
+          array.splice(index,1);
+        }
+      })
+      console.log(arrProductos);
+      localStorage.setItem("producto", JSON.stringify(arrProductos));
+      crearTablaProducto();
+
+  })
+
+const crearTablaProducto = () => {
+  const contenidoProductos = document.querySelector("#contenidoProductos");
+  contenidoProductos.innerHTML = "";
+
+  arrProductos.forEach((item, index, array) => {
+    const tr = document.createElement("TR");
+    const td1 = document.createElement("TD");
+    const td2 = document.createElement("TD");
+    const td3 = document.createElement("TD");
+    const button = document.createElement("BUTTON");
+
+    button.setAttribute("id", item.id);
+    button.innerHTML = "Borrar";
+    td1.innerHTML = item.nombre;
+    td2.innerHTML = item.cantidad;
+
+    td3.append(button);
+    tr.append(td1, td2, td3);
+    contenidoProductos.append(tr);
+  })
+}
 
 const eliminarProducto = () => {
   
 }
+
+crearTablaProducto();
+eliminarProducto();
+
 
 const agregarProducto = () => {
   
