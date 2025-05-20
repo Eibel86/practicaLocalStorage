bced983cfbbe0fd90d115ada5c5bfa087f8997b1
Eibel86 <afc72455@gmail.com>
2025-05-20 14:01:26 +0200

    Terminado almacenamiento local

--- js/main.js
+++ js/main.js
@@ -1,83 +1,48 @@
-//////////////
+
+const formulario = document.querySelector("#formulario");
+let nombreProducto = "";
+let idProducto= "";
+let arrProductos = JSON.parse(localStorage.getItem("producto")) || [];
+
+let objProductos = {
+  id: 1,
+  nombre: "",
+  cantidad: 1
+}
+
+
+formulario.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+ // console.log(ev.target.nombre_producto.value);
+  nombreProducto = ev.target.nombre_producto.value;
+  let anadirgion = "-";
+  idProducto = nombreProducto.split(" ").join("-") + anadirgion;
+  console.log("idProducto: ", idProducto);
+  let mached = false;
+  arrProductos.forEach((item) => {
+    console.log(item.nombre);
+    if (item.nombre === nombreProducto){
+      item.cantidad += 1;
+      mached = true;
+      console.log(item.cantidad);
+    } 
+  })
+
+  if (!mached) {
+    objProductos = {
+      id:idProducto,
+      nombre: nombreProducto,
+      cantidad:1
+    }
+    arrProductos.push(objProductos);
+  } else {
+    console.log("nombre coincide, no se crea objeto")
+  }
+  localStorage.setItem("producto", JSON.stringify(arrProductos)); 
+})
 
 
 
-
-//////////KATAS
-
-//1
-// Description
-// Your job is to write a simple password validation function, as seen on many websites.
-
-// The rules for a valid password are as follows:
-
-// There needs to be at least 1 uppercase letter.
-// There needs to be at least 1 lowercase letter.
-// There needs to be at least 1 number.
-// The password needs to be at least 8 characters long.
-// Your function takes a string argument and returns whether it is a valid password, as a boolean.
-
-// Examples:
-// "Abcd1234" ===> true
-// "Abcd123" ===> false
-// "abcd1234" ===> false
-// "AbcdefGhijKlmnopQRsTuvwxyZ1234567890" ===> true
-// "ABCD1234" ===> false
-// "Ab1!@#$%^&*()-_+={}[]|\:;?/>.<," ===> true;
-// "!@#$%^&*()-_+={}[]|\:;?/>.<," ===> false;
-
-// function password(str) {
-//     //validate password
-// }
-
-// Descripción:
-// Existe una secuencia de números que sigue el patrón
-
-//           1
-//          11
-//          21
-//         1211
-//        111221
-//        312211
-//       13112221
-//      1113213211
-//           .
-//           .
-//           .
-// Comenzando con "1" las siguientes líneas se producen diciendo lo que ves", de modo que la línea dos es "uno uno", la línea tres es "dos uno(s)", la línea cuatro es "uno dos uno uno".
-
-// Escriba una función que, dado un valor inicial como una cadena, devuelva la secuencia adecuada como una lista. El valor inicial puede tener cualquier número de dígitos. La condición de terminación está definida por el número máximo de iteraciones, que también se proporciona como argumento.
-
-// function lookAndSay(data,len){
-//   // Populate result list with the look and say numbers
-//   // data:  starting number set
-//   // len:   sequence length
-// }
-
-
-
-
-let test = "aB23"
-// let re = /^[A-Za-z0-9]+$/.test(test);
-// let so = (/[A-Za-z]/.test(test) || /\d/.test(test));
-// console.log(re);// true
-// console.log(so);// true
-// let nu = (test.length > 3 || test.length < 20);
-// console.log(nu);// true
-
-
-
-  const valid = (/^[A-Za-z0-9]+$/.test) && (/[A-Za-z]/.test || /\d/.test) && (test.length > 3 || test.length < 20);
-            console.log(valid);
-            if ( valid === true) 
-              return valid = "VALID";
-            else 
-              return valid = "INVALID";
+const agregarProducto = () => {
   
-
-
-    console.log(valid);
-
-
-
-   
+  }
